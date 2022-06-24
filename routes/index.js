d1c3ffa4310542e47659e10ee57cd5b0a0ad8ecd
ariefrenaldy <arief.renaldy16@gmail.com>
2022-06-24 10:43:05 +0700

    add comment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
+let comments = [];
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "M Arief Renaldy!" });
+router.get("/", (req, res, next) => {
+  res.render("index", { comments });
+});
+
+/* POST increase counter by 1. */
+router.post("/add-comment", function (req, res, next) {
+  const { nama, text } = req.body;
+  comments.splice(0, 0, { nama, text, date: new Date() });
+  console.log(comments);
+  res.redirect("/");
 });
 
 module.exports = router;
